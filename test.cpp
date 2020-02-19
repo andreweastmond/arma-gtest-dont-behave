ca534c37058de032f4a56227de9d14463fce986d
Andrew Eastmond <andy.dev.eastmond@gmail.com>
2020-02-19 13:42:53 +0000

    Made more minimal

--- test.cpp
+++ test.cpp
@@ -1,13 +1,10 @@
 #include "gtest/gtest.h"
 
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
 
-
 TEST(example_test, example_test) {
     ASSERT_EQ(1, 1);
 }
-
