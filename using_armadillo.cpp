8f51c2f660d18d9833eac488b0f11a834d0d7024
Robert Johnson <robert.johnson@kindredgroup.com>
2020-02-19 11:32:12 +0000

    Initial commit.

---
+++ using_armadillo.cpp
@@ -0,0 +1,17 @@
+#define ARMA_NO_DEBUG
+
+#include <armadillo>
+#include <stdexcept>
+#include <iostream>
+#include <cmath>
+#include <deque>
+#include <map>
+
+using namespace std;
+using namespace arma;
+
+
+void arma_test() {
+    Mat<double> testMatrix(2, 2, fill::zeros);
+}
+
